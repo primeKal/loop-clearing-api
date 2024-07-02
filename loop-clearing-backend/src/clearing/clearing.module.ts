dce3c5e3d43cdeddd295e3d23da123e1e63841fd
Kaleb Teshale <kalebteshale72@gmail.com>
2024-07-02 11:16:36 +0300

    Fix: Backend: bug fix on importing clearing

--- loop-clearing-backend/src/clearing/clearing.module.ts
+++ loop-clearing-backend/src/clearing/clearing.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ClearingService } from './clearing.service';
 import { ClearingController } from './clearing.controller';
+import { Clearing } from './entities/clearing.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [ClearingController],
   providers: [ClearingService],
+  imports: [TypeOrmModule.forFeature([Clearing])]
+
 })
-export class ClearingModule {}
+export class ClearingModule { }
