4ee56edd5ead158851e86cdb527593c85fcd46ce
Kaleb Teshale <kalebteshale72@gmail.com>
2024-07-04 08:19:27 +0300

    Fix: Frontend api change

--- loop-clearing-frontend/src/EndPoints.jsx
+++ loop-clearing-frontend/src/EndPoints.jsx
@@ -1,3 +1,3 @@
 // export const baseUrl = process.env.BASEURL? process.env.BASEURL : "http://localhost:8000/"
-// export const baseUrl = "https://ffdb.onrender.com/"
-export const baseUrl = "http://localhost:8000/"
+export const baseUrl = "https://loop-clearing-api.onrender.com";
+// export const baseUrl = "http://localhost:8000/"
